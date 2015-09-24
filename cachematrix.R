00a4e578336442b6e8a0a3cf688695b8a4ab725f
Agent-Sesame <a.l.julian@gmail.com>
2015-09-24 10:58:03 -0700

    Revised sample for homework
    Revised sample code for homework assignment as follows 1) wrote
    makeMatrix to quickly make matrix test object, 2) rewrote
    makecacheMatrix to cache the inverse of matrix supplied by #1, 3)
    rewrote cacheSolve to calculation the inverse of matrix if not already
    cached.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,101 @@
-## Put comments here that give an overall description of what your
-## functions do
+## If function makecacheMatrix supplied with pre-existing matrix 'x' from global
+## environment it will cache the inverse of 'x' using the 'solve' function.
+## Quickly create a text inversable matrix using a 3rd function 'makeMatrix'.
 
-## Write a short comment describing this function
+## Example matrix 'x' supplied to makecacheMatrix: 
+##      [,1] [,2]
+## [1,]    1    3
+## [2,]    2    4
 
-makeCacheMatrix <- function(x = matrix()) {
+## Expected inverse of 'x' output from cacheSolve:
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
 
+makecacheMatrix <- function(x = matrix()) {
+        
+        ## Set variable 'm' representing matrix inverse to NULL in parent 
+        ## environment.
+        
+        m <- NULL
+        
+        ## In parent environment set variable 'set' to a function, define that
+        ## function's current environment to 1) replace 'x' with 'y' in the 
+        ## main function makecacheMatrix parent environment, and 2) sets 
+        ## variable 'm' to NULL in parent environment.
+        
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        ## In parent environment set variable 'get' to a function, defining that
+        ## function to return 'x'.
+        
+        get <- function() x
+        
+        ## In parent environment set variable 'setinverse' to the function
+        ## 'solve' and set variable 'm' from current environment to 'solve' in
+        ## parent environment.
+        
+        setinverse <- function(solve) m <<- solve
+        
+        ## In parent environment set variable 'getinverse' to function that
+        ## returns value of 'm'.
+        
+        getinverse <- function() m
+        
+        ## Listify all parent environment variables established above.
+        
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+        
 }
 
-
-## Write a short comment describing this function
+## Function intends to take a argument 'x' defined as matrix output from 
+## function 'makeCacheMatrix( )' and solve the inverse of that matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        ## In parent environment set variable 'm' to stored function $getinverse
+        
+        m <- x$getinverse()
+        
+        ## If in parent environmnet 'm' is not null, display message on screen
+        ## and return cached value 'm'.
+        
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        ## In parent environment set variable 'data' to stored function 
+        ## $get, the stored matrix.
+        
+        data <- x$get()
+        
+        ## In parent environment set variable 'm' to the function solve() with
+        ## the variable 'data' as argument.
+        
+        m <- solve(data)
+        
+        ## In global or parent environmet (?) call listed function 'setinverse'.
+        
+        x$setinverse(m)
+        
+        ## Return 'm'
+        
+        m
+}
+
+makeMatrix <- function(mdim = 2) {
+        
+        ## Quickly generate test object in global environment using argument
+        ## 'mdim' as variable for matrix data, row, and column definition.
+        
+        x <<- matrix(rnorm(mdim ^ 2), nrow = mdim, ncol = mdim)
+        
+        return (x)
+        
 }
